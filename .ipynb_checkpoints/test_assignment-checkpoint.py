b7252ea77e9ee45ae540ffc0c31f65169bace342
twiga2 <machuved@gmail.com>
2025-01-29 18:21:26 +0300

    week 4 submission

--- .ipynb_checkpoints/test_assignment-checkpoint.py
+++ .ipynb_checkpoints/test_assignment-checkpoint.py
@@ -4,19 +4,19 @@ class TestControlStructures(unittest.TestCase):
 
     def test_while_loop(self):
         # Test if the while loop prints even numbers up to 20 and stops at 16
-        self.assertEqual
+        pass
 
     def test_for_loop_continue(self):
         # Test if the for loop skips numbers divisible by 3
-        self.assertEqual
+        pass
 
     def test_if_else(self):
         # Test if the if-else statement categorizes numbers correctly
-        self.assertEqual
+        pass
 
     def test_nested_loops(self):
         # Test if the nested loops generate the correct multiplication table
-        self.assertEqual
+        pass
 
 if __name__ == "__main__":
     unittest.main()
